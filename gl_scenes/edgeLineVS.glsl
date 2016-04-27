13d6d5282ae913d164f0281d431bf13ca9170689
Silvio Delgado <silviodelgado70@gmail.com>
2016-04-27 11:31:08 -0700

    - Edge Detection & Hough Transform in CPU is ready

--- gl_scenes/edgeLineVS.glsl
+++ gl_scenes/edgeLineVS.glsl
@@ -6,24 +6,25 @@ void main(void)
 
     float cos_cita = col.g/col.r;
     float sin_cita = col.b/col.r;
+    float tan_cita = col.b/col.g;
 
     float d = vertex.x * cos_cita + vertex.y * sin_cita;
 
-    if(col.r == 0. || col.r < 0.1)
+    if(col.r == 0. || col.r < 0.85)
     {
         cos_cita = 10.0;
         d = 10.0;
     }
 
-    gl_PointSize = col.r * 10.;
+    gl_PointSize = col.r * 5.;
 
     if(col.a == 1.)
     {
-        gl_Position = vec4(-1. + cos_cita, -1. + d * 2.,0.0,1.0);
+        gl_Position = vec4(-1. + tan_cita, -1. + d * 2.,0.0,1.0);
     }
     else
     {
-        gl_Position = vec4(cos_cita, -1. + d * 2.,0.0,1.0);
+        gl_Position = vec4(tan_cita, -1. + d * 2.,0.0,1.0);
     }
 
 }
--- main.cpp
+++ main.cpp
@@ -23,10 +23,13 @@ int edgeDraw(RASPITEX_STATE *raspitex_state);
 
 void edgeLineDetect(RASPITEX_STATE *state);
 
+void cpuAndGpu(RASPITEX_STATE *state);
+void cpuAndGpuOnlyTransfer(RASPITEX_STATE *state);
+
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 int RENDER_STEPS;
 
-int render_id = 0;
+int render_id = 6;
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 RaspisilvioShaderProgram edgeLineShader = {
         .vs_file = "gl_scenes/edgeLineVS.glsl",
@@ -167,7 +170,10 @@ GLuint houghTexId;
 
 GLuint houghFBId;
 
-static const char *const fileToLoad = "./Experiments/Edge1/step0.tga";
+static const char *const fileToLoad = "./Experiments/Edge3/step0.tga";
+Mat src, grad_x, grad_y, abs_grad_x, abs_grad_y, grad, grad_t, fromGPU, gpusobel;
+vector<Vec2f> lines;
+uint8_t *pixels_from_fb;
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 int main() {
@@ -212,15 +218,21 @@ int main() {
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 void edgePrintStep() {
-    RENDER_STEPS = 6;
+    RENDER_STEPS = 12;
     static char *render_steps[] = {
             "0 - > Camera",
-            "2 - > Sobel",
+            "1 - > Sobel",
             "2 - > Sobel X",
             "3 - > Sobel Y",
             "4 - > Sobel MXY",
             "5 - > Sobel MDS",
-            "6 - > Hough"
+            "6 - > Hough",
+            "7 - > CPU Sobel",
+            "8 - > CPU Hough",
+            "9 - > GPU Sobel + Threshold + Transfer CPU",
+            "10- > GPU Sobel + Threshold + Transfer CPU & Hough",
+            "11- > CPU Only Hough with result of CPU Sobel",
+            "12- > CPU Only Hough with result of GPU Sobel",
     };
     printf("Render id = %d\n%s\n", render_id, render_steps[render_id]);
 }
@@ -249,6 +261,13 @@ int edgeInit(RASPITEX_STATE *raspitex_state) {
     raspisilvioLoadTextureFromFile(fileToLoad, &yImageTexId);
 
     raspisilvioCreateTextureFB(&houghTexId, binsAngle, binsDistance, NULL, GL_RGBA, &houghFBId);
+
+    src = imread("./Experiments/Edge3/step0.jpg", CV_LOAD_IMAGE_GRAYSCALE);
+    fromGPU = Mat::zeros(Size(src.cols, src.rows), CV_8UC1);
+
+    gpusobel = imread("./Experiments/Edge3/gpu.jpg");
+
+    raspisilvioCreateTextureData(&pixels_from_fb, raspitex_state->width, raspitex_state->height, GL_RGBA);
 
     return rc;
 }
@@ -305,11 +324,112 @@ int edgeDraw(RASPITEX_STATE *state) {
                 raspisilvioSaveToFile(state, "step5.tga");
             }
             break;
+        case 7:
+            Sobel(src, grad_x, CV_16S, 1, 0, 3, 1, 0, BORDER_DEFAULT);
+            convertScaleAbs(grad_x, abs_grad_x);
+            Sobel(src, grad_y, CV_16S, 0, 1, 3, 1, 0, BORDER_DEFAULT);
+            convertScaleAbs(grad_y, abs_grad_y);
+            addWeighted(abs_grad_x, 0.5, abs_grad_y, 0.5, 0, grad);
+            break;
+        case 8:
+            Sobel(src, grad_x, CV_16S, 1, 0, 3, 1, 0, BORDER_DEFAULT);
+            convertScaleAbs(grad_x, abs_grad_x);
+            Sobel(src, grad_y, CV_16S, 0, 1, 3, 1, 0, BORDER_DEFAULT);
+            convertScaleAbs(grad_y, abs_grad_y);
+            addWeighted(abs_grad_x, 0.5, abs_grad_y, 0.5, 0, grad);
+            threshold(grad, grad_t, 20, 255, THRESH_BINARY);
+            HoughLines(grad_t, lines, 1, CV_PI / 180, 200, 0, 0);
+            break;
+        case 9:
+            cpuAndGpuOnlyTransfer(state);
+            break;
+        case 10:
+            cpuAndGpu(state);
+            break;
+        case 11:
+            HoughLines(grad_t, lines, 1, CV_PI / 180, 200, 0, 0);
+            break;
+        case 12:
+            HoughLines(fromGPU, lines, 1, CV_PI / 180, 200, 0, 0);
+            break;
     }
 
     return 0;
 }
 
+///////////////////////////////////////////////////////////////////////////////////////////////////
+void cpuAndGpuOnlyTransfer(RASPITEX_STATE *state) {
+    static int x = 0;
+    raspisilvioProcessingTexture(&sobel_shader_t, state, FRAMBE_BUFFER_PREVIEW, yImageTexId);
+
+    GLCHK(glReadPixels(0, 0, state->width, state->height, GL_RGBA, GL_UNSIGNED_BYTE, pixels_from_fb));
+
+    uint8_t *out = pixels_from_fb;
+
+    for (int i = 0; i < src.rows; i++) {
+        for (int j = 0; j < src.cols; j++) {
+
+            if (out[0] > 100) {
+                fromGPU.at<uchar>(i, j) = 255;
+            }
+            else {
+                fromGPU.at<uchar>(i, j) = 0;
+            }
+
+            out += 4;
+        }
+    }
+
+    if (x == 0) {
+        imwrite("gpu.jpg", fromGPU);
+        x = 1;
+    }
+}
+///////////////////////////////////////////////////////////////////////////////////////////////////
+void cpuAndGpu(RASPITEX_STATE *state) {
+    static int x = 0;
+    raspisilvioProcessingTexture(&sobel_shader_t, state, FRAMBE_BUFFER_PREVIEW, yImageTexId);
+
+    GLCHK(glReadPixels(0, 0, state->width, state->height, GL_RGBA, GL_UNSIGNED_BYTE, pixels_from_fb));
+
+    uint8_t *out = pixels_from_fb;
+
+    for (int i = 0; i < src.rows; i++) {
+        for (int j = 0; j < src.cols; j++) {
+
+            if (out[0] > 100) {
+                fromGPU.at<uchar>(i, j) = 255;
+            }
+            else {
+                fromGPU.at<uchar>(i, j) = 0;
+            }
+
+            out += 4;
+        }
+    }
+
+
+    HoughLines(fromGPU, lines, 1, CV_PI / 180, 250, 0, 0);
+
+    if (x == 0) {
+        Mat final;
+        final = imread("./Experiments/Edge3/step0t.jpg");
+        for( size_t i = 0; i < lines.size(); i++ )
+        {
+            float rho = lines[i][0], theta = lines[i][1];
+            Point pt1, pt2;
+            double a = cos(theta), b = sin(theta);
+            double x0 = a*rho, y0 = b*rho;
+            pt1.x = cvRound(x0 + 1000*(-b));
+            pt1.y = cvRound(y0 + 1000*(a));
+            pt2.x = cvRound(x0 - 1000*(-b));
+            pt2.y = cvRound(y0 - 1000*(a));
+            line( final, pt1, pt2, Scalar(0,0,255), 3);
+        }
+        imwrite("final.jpg", final);
+        x = 1;
+    }
+}
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 void edgeLineDetect(RASPITEX_STATE *state) {
     raspisilvioProcessingTexture(&sobelMDS_shader_t, state, sobelFBId, yImageTexId);
